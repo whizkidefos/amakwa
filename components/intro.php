bb97d0f3ff6851584e0af6540a948e80c86f0fb1
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-09-16 15:36:41 +0100

    building insight section

--- components/intro.php
+++ components/intro.php
@@ -1,1 +1,14 @@
-<h1>Intro</h1>
+<section class="intro">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m uk-flex-middle" uk-grid>
+            <figure>
+                <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
+            </figure>
+            <article>
+                <h2 class="section-heading">OUR STORY (History of Amakwa Unity Model Secondary School)</h2>
+                <p>Amakwa Unity Model Secondary School was established by visionary members of the Amakwa Progressive Foundation with the noble aim of bringing quality education closer to their community's younger generation. Recognizing the challenges within the Nigerian educational system, these committed individuals came together to contribute to the founding of the school. The construction of the school's infrastructure commenced approximately six to seven years ago, while her first students were enrolled 1st September 2018. </p>
+                <a href="/about" class="nav-pills">Learn more</a>
+            </article>
+        </div>
+    </div>
+</section>
--- components/our-services.php
+++ components/our-services.php
@@ -1,1 +1,44 @@
-<h1>Our services</h1>
+<section class="our-services">
+    <div class="uk-container">
+        <h2 class="uk-text-center section-heading">Insight</h2>
+        <br><br>
+        <div class="uk-child-width-1-2@m uk-child-width-1-4@l uk-grid-column-small uk-grid-match" uk-grid>
+            <div class="">
+                <article class="service-card">
+                    <span uk-icon="icon: future" class="service-card-icon"></span>
+                    <h4>Our Mission</h4>
+                    <p>
+                        To provide accessible and quality education, empowering students with knowledge, skills, and values for a brighter future.
+                    </p>
+                </article>
+            </div>
+
+            <div class="">
+                <article class="service-card">
+                    <span uk-icon="icon: eye" class="service-card-icon"></span>
+                    <h4>Our Vision</h4>
+                    <p>
+                        Become the leading institution in Nigeria, known for academic excellence, character development, and community impact.
+                    </p>
+                </article>
+            </div>
+
+            <div class="">
+                <article class="service-card">
+                    <span uk-icon="icon: crosshairs" class="service-card-icon"></span>
+                    <h4>Faculty and staff</h4>
+                    <p>To provide accessible and quality education, empowering students with knowledge, skills, and values for a brighter future.</p>
+                </article>
+            </div>
+
+            <div>
+                <article class="service-card">
+                    <span uk-icon="icon: users" class="service-card-icon"></span>
+                    <h4>School policies</h4>
+                    <p>To provide accessible and quality education, empowering students with knowledge, skills, and values for a brighter future.</p>
+                </article>
+            </div>
+
+        </div>
+    </div>
+</section>
