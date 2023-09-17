2d120bf5a00c7cf46b5f620436c11923a718ca30
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-09-17 14:55:31 +0100

    building contact page

--- page.php
+++ page.php
@@ -11,10 +11,10 @@
 <main class="page-content">
     <div class="uk-container">
         <div class="uk-child-with-expand@s" uk-grid>
-            <div class="uk-width-2-3">
+            <div class="uk-width-2-3@m">
                 <?php the_content(); ?>
             </div>
-            <div class="uk-width-1-3">
+            <div class="uk-width-1-3@m">
                 <h3>Quick links</h3>
                 <p>
                     Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
