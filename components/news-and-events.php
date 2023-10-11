578e955da6fc312cf8c390cea4c3356f3d77c5fc
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-10-11 16:35:30 +0100

    Footer, News section, faq page tweaks

--- components/news-and-events.php
+++ components/news-and-events.php
@@ -1,56 +1,68 @@
 <section class="news-and-events">
     <div class="uk-container">
         <h2 class="uk-text-center section-heading">News & Events</h2>
-        <div class="uk-child-width-1-3@m" uk-grid>
+        <br><br>
+        <div class="uk-child-width-1-3@m uk-grid-match" uk-grid>
             <div>
-            <article class="news-and-events-card">
-                <figure class="news-and-events-image">
-                    <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
-                    <div class="news-and-events-meta">
-                        <small>Category title</small>
-                        <small>10/09/2024</small>
+                <article class="news-and-events-card">
+                    <figure class="news-and-events-image">
+                        <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
+                        <div class="news-and-events-meta">
+                            <small>Category title | </small>
+                            <small>10/09/2024</small>
+                        </div>
+                    </figure>
+                    <div class="news-and-events-content">
+                        <h4 class="news-and-events-title">Event Title</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <div class="news-and-events-cta">
+                            <a href="#" class="amakwa-button">Learn more</a>
+                        </div>
                     </div>
-                </figure>
-                <div class="news-and-events-content">
-                    <h4 class="news-and-events-title">Title</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                <br>
-                <a href="#" class="amakwa-button">Learn more</a>
-            </article>
+                </article>
             </div>
 
-            <article class="news-and-events-card">
-                <figure class="news-and-events-image">
-                    <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
-                    <div class="news-and-events-meta">
-                        <small>Category title</small>
-                        <small>10/09/2024</small>
-                    </div>
-                </figure>
-                <div class="news-and-events-content">
-                    <h4 class="news-and-events-title">Title</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                <br>
-                <a href="#" class="amakwa-button">Learn more</a>
-            </article>
+            <div>
+                <article class="news-and-events-card">
+                    <figure class="news-and-events-image">
+                        <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
+                        <div class="news-and-events-meta">
+                            <small>Category title | </small>
+                            <small>10/09/2024</small>
+                        </div>
+                    </figure>
+                    <div class="news-and-events-content">
+                        <h4 class="news-and-events-title">News Title</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
 
-            <article class="news-and-events-card">
-                <figure class="news-and-events-image">
-                    <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
-                    <div class="news-and-events-meta">
-                        <small>Category title</small>
-                        <small>10/09/2024</small>
+                        <div class="news-and-events-cta">
+                            <a href="#" class="amakwa-button">Learn more</a>
+                        </div>
                     </div>
-                </figure>
-                <div class="news-and-events-content">
-                    <h4 class="news-and-events-title">Title</h4>
-                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                </div>
-                <br>
-                <a href="#" class="amakwa-button">Learn more</a>
-            </article>
+                </article>
+            </div>
+
+            <div>
+                <article class="news-and-events-card">
+                    <figure class="news-and-events-image">
+                        <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
+                        <div class="news-and-events-meta">
+                            <small>Category title | </small>
+                            <small>10/09/2024</small>
+                        </div>
+                    </figure>
+                    <div class="news-and-events-content">
+                        <h4 class="news-and-events-title">Announcement Title</h4>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+
+                        <div class="news-and-events-cta">
+                            <a href="#" class="amakwa-button">Learn more</a>
+                        </div>
+                    </div>
+                </article>
+            </div>
+
         </div>
     </div>
 </section>
--- page-faqs.php
+++ page-faqs.php
@@ -8,13 +8,13 @@
     </div>
 </section>
 
-<main class="page-content">
+<main class="page-content faq-page-content">
     <div class="uk-container">
         <div class="uk-child-with-expand@s" uk-grid>
             <div class="uk-width-2-3@m">
             <ul uk-accordion>
                 <li>
-                    <a class="uk-accordion-title" href>Question example 1</a>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quisquam, totam.</a>
                     <div class="uk-accordion-content">
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
@@ -22,7 +22,55 @@
                     </div>
                 </li>
                 <li>
-                    <a class="uk-accordion-title" href>Question example 2</a>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
+                    <div class="uk-accordion-content">
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
+                        </p>
+                    </div>
+                </li>
+                <li>
+                    <a class="uk-accordion-title" href>Lorem ipsum dolor sit amet.</a>
                     <div class="uk-accordion-content">
                         <p>
                             Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
@@ -32,10 +80,18 @@
             </ul>
             </div>
             <div class="uk-width-1-3@m">
-                <h3>Quick links</h3>
-                <p>
-                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta impedit quasi accusantium expedita necessitatibus soluta officia, repellendus animi reprehenderit sit.
-                </p>
+                <div class="quick-links">
+                    <img src="<?php echo get_template_directory_uri() . "/images/students.jpg" ?>" alt="amakwa students">
+                    <div class="quick-links-content">
+                        <h3>Quick links</h3>
+                        <hr>
+                        <a class="quick-link" href="/faqs"><span uk-icon="question"></span> Frequently asked questions</a>
+                        <a class="quick-link" href="/news-events"><span uk-icon="info"></span> News & events</a>
+                        <a class="quick-link" href="/about#principals-note"><span uk-icon="comment"></span> Principals statement</a>
+                        <a class="quick-link" href="/admissions"><span uk-icon="copy"></span> Admissions</a>
+                        <a class="quick-link" href="/academics"><span uk-icon="bookmark"></span> Academics</a>
+                    </div>
+                </div>   
             </div>
         </div>
     </div>
