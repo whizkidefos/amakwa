a71b4c6d2e1fc5c16f98cfb4c29359fae24c95a0
Efosa Igbinehi <whizkidefos@users.noreply.github.com>
2023-10-22 09:15:21 +0100

    Made homepage dynamic

--- components/intro.php
+++ components/intro.php
@@ -5,8 +5,8 @@
                 <img src="<?php echo get_template_directory_uri() . "/images/intro.jpg" ?>" alt="intro-image">
             </figure>
             <article>
-                <h2 class="section-heading">OUR STORY</h2>
-                <p>Amakwa Unity Model Secondary School was established by visionary members of the Amakwa Progressive Foundation with the noble aim of bringing quality education closer to their community's younger generation. Recognizing the challenges within the Nigerian educational system, these committed individuals came together to contribute to the founding of the school. The construction of the school's infrastructure commenced approximately six to seven years ago, while her first students were enrolled 1st September 2018. </p>
+                <h2 class="section-heading"><?php the_field('our_story_title'); ?></h2>
+                <p><?php the_field('our_story_description'); ?></p>
                 <a href="/about" class="amakwa-button">Learn more</a>
             </article>
         </div>
